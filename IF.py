b40d594244efc47254b68ecd20e4db413f380cc3
cyper_wolf <140687033+max97268@users.noreply.github.com>
2024-01-12 14:02:56 +0500

    Add files via upload

---
+++ IF.py
@@ -0,0 +1,41 @@
+#1-exr
+"""
+a = int(input('give me a number: '))
+if a > 0:
+        print(a+1)
+elif a < 0:
+        print(a / 2)
+elif a == 0:
+        print(a)
+"""
+#2-exr
+"""
+a = int(input('first number: '))
+b = int(input('second number: '))
+c = int(input('third number: '))
+
+if a > 0 and b < 0 and c < 0 or a < 0 and b > 0 and c < 0 or a < 0 and b < 0 and c > 0:
+    print('there are one postive number and two negative number!')
+elif a > 0 and b > 0 and c < 0 or a < 0 and b > 0 and c > 0 or a > 0 and b < 0 and c > 0:
+    print('there are two postive number and one negative number!')
+elif a and b and c > 0:
+    print('all of them are positive number!')
+elif a and b and c < 0:
+    print('all of them are negative number!')
+"""
+#3-exr
+"""
+a = int(input('first one: '))
+b = int(input('second one: '))
+if a > b:
+    print(f'a is bigger than b')
+elif a < b:
+    print(f'b is bigger than a')
+elif a == b:
+    print('both of are equal')
+"""
+
+
+
+        
+
